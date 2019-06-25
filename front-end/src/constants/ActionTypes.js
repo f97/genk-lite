e3b462fb153678768caede7d120556a34e762a3b
khoanhd7059 <khoanhd7059@gmail.com>
2019-06-25 11:18:29 +0700

    Remove excess file

--- front-end/src/constants/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const CHANGE_COLOR = 'CHANGE_COLOR';
-export const CHANGE_SIZE = 'CHANGE_SIZE';
-export const CHANGE_FONT = 'CHANGE_FONT';
---
+++ front-end/src/genk.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Genk() {
+  return (
+    <div className="App">
+      <p>Genk</p>
+    </div>
+  );
+}
+
+export default Genk;
